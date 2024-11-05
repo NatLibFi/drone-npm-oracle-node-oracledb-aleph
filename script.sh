0b73a576a051d536f415a06377f623adcabe3115
Minttu Hurme <ammsalme@users.noreply.github.com>
2024-11-05 17:07:15 +0200

    npm publish in githubActions

--- script.sh
+++ script.sh
@@ -44,9 +44,10 @@ echo "Applying patches"
 patch -p0 < ../fix-name.patch
 patch -p0 < ../fix-charset.patch
 
-echo "Building package"
-npm install
-npm run buildbinary
-npm run buildpackage
-
-npm publish "natlibfi-oracledb-aleph-${PACKAGE_VERSION}.tgz"
+# this is done in guthubActions
+#echo "Building package"
+#npm install
+#npm run buildbinary
+#npm run buildpackage
+#
+#npm publish "natlibfi-oracledb-aleph-${PACKAGE_VERSION}.tgz"
